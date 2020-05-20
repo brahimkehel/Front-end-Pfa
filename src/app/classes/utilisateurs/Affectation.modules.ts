2f80ae4dddb924398e07ac212dc459ec844ed3a8
brahim kehel <brahimkehel@gmail.com>
2020-05-20 23:18:34 +0000

    Seance/Affectation CRUUUUUUD

---
+++ src/app/classes/utilisateurs/Affectation.modules.ts
@@ -0,0 +1,8 @@
+export class Affectation {
+    idFiliere?:Int16Array;
+    idMatiere:string;
+    idEns:string;
+    libelle_Filiere:string;
+    libelle_Matiere:string;
+    libelle_Enseignant:string;
+}
